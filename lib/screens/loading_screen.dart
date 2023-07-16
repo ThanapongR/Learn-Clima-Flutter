3181a63441ea00079947218ab9e3543d64d4d4b7
thanapongr <thanapong.rodruang@gmail.com>
2023-07-16 14:42:51 +0700

    Add weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:clima/services/location.dart';
+import 'package:clima/utilities/secret.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,6 +13,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   Future<void> getLocation() async {
@@ -18,6 +21,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getLocation();
     print(location.latitude);
     print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=13.79&lon=100.52&appid=$kOpenWeatherKey');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
