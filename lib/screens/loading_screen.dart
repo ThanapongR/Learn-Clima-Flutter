68d6c0fc701f40fa9478ba9ca567273994c22009
thanapongr <thanapong.rodruang@gmail.com>
2023-07-16 15:14:23 +0700

    Add network helper class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:clima/utilities/secret.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,37 +11,35 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
-    getData();
+    getLocationData();
   }
 
-  Future<void> getLocation() async {
+  Future<void> getLocationData() async {
     Location location = Location();
     await location.getLocation();
-  }
 
-  void getData() async {
-    http.Response response = await http.get(
-        'https://api.openweathermap.org/data/2.5/weather?lat=13.79&lon=100.52&appid=$kOpenWeatherKey');
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-    if (response.statusCode == 200) {
-      String data = response.body;
+    NetworkHelper networkHelper = NetworkHelper(
+        url:
+            'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kOpenWeatherKey');
 
-      var decodedData = jsonDecode(data);
+    dynamic weatherData = await networkHelper.getData();
 
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
+    double temperature = double.parse(weatherData['main']['temp'].toString());
+    int condition = weatherData['weather'][0]['id'];
+    String cityName = weatherData['name'];
 
-      print(temperature);
-      print(condition);
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
+    print(temperature);
+    print(condition);
+    print(cityName);
   }
 
   @override
